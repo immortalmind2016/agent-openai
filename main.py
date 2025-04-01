4bd74911a4de250535c5e812342bea4b81236829
Mohamed Salah <immortal.mind2016@gmail.com>
2025-04-02 00:26:34 +0200

    feat: introduce describe tables tool name

--- main.py
+++ main.py
@@ -2,26 +2,37 @@ from langchain_openai import ChatOpenAI
 from langchain.prompts import (
     ChatPromptTemplate,
     HumanMessagePromptTemplate,
-    MessagesPlaceholder
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate
 )
 
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
 from dotenv import load_dotenv
+from langchain.schema import SystemMessage
 
-from tools.sql import run_query_tool
+from tools.sql import run_query_tool,describe_table_tool
 
 load_dotenv()
 
 chat=ChatOpenAI()
 
+def list_tables() -> str:
+    c=conn.cursor()
+    c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    row = c.fetchall()
+    return "\n".join([table[0] for table in row])
 prompt = ChatPromptTemplate(
     messages=[
+        SystemMessage("You are a SQL expert. You can run SQL queries on the example.db database.\n"
+                      f"the database has the following tables: {list_tables()}\n"
+                      "Don't make assumptions about the table names."
+                      "If you need to know the table structure, use the 'describe_table' function."),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad")
     ]
 
 )
-
+tools=[run_query_tool,describe_table_tool]
 agent=OpenAIFunctionsAgent(
     llm=chat,
     tools=[run_query_tool],
--- tools/sql.py
+++ tools/sql.py
@@ -17,3 +17,15 @@ run_query_tool = Tool.from_function(
     name="run_sql_query",
     description="Run a SQL query on the example.db database. The query should be a valid SQL statement.",
 )
+
+def describe_table(tables_name) -> str:
+    c=conn.cursor()
+    tables=', '.join("'"+table[0]+"'" for table in tables_name)
+    rows =c.execute(f"SELECT sql FROM sqlite_master WHERE type='table' AND name IN ({tables});")
+    return '\n'.join([row[0] for row in rows if row[0] is not None])
+
+describe_table_tool = Tool.from_function(
+    func=describe_table,
+    name="describe_table",
+    description="Describe the table structure of the example.db database. The table name should be a valid SQL statement.",
+)
