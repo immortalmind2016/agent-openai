8a8e1df5e7f16140e70662d7142f1f6168c88eee
Mohamed Salah <immortal.mind2016@gmail.com>
2025-04-02 02:34:13 +0200

    feat: memory for AI results

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ from langchain.prompts import (
     ChatPromptTemplate,
     HumanMessagePromptTemplate,
     MessagesPlaceholder,
-    SystemMessagePromptTemplate
 )
 
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
 from dotenv import load_dotenv
 from langchain.schema import SystemMessage
 from tools.report import write_report_tool
-
+from langchain.memory import ConversationBufferMemory
 
 from tools.sql import run_query_tool,list_tables,describe_tables_tool
 
@@ -27,11 +26,13 @@ prompt = ChatPromptTemplate(
             "Do not make any assumptions about what tables exist "
             "or what columns exist. Instead, use the 'describe_tables' function"
         )),
+        MessagesPlaceholder(variable_name="chat_history"),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad")
     ]
 )
 
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 tools = [
     run_query_tool,
     describe_tables_tool,
@@ -47,8 +48,14 @@ agent = OpenAIFunctionsAgent(
 agent_executor = AgentExecutor(
     agent=agent,
     verbose=True,
-    tools=tools
+    tools=tools,
+    memory=memory
 )
 
-agent_executor("Summarize the top 5 most popular products. Write the results to a report file.")
-# agent_executor("how many users are there?")
+agent_executor(
+    "How many orders are there? Write the result to an html report."
+)
+
+agent_executor(
+    "Repeat the exact same process for users and create a new file called users.html."
+)
